8b5bb34f398dbfe7340e7556089e00026f8963ae
majkelp1982 <s.pomazanka@gmail.com>
2024-12-01 21:17:47 +0100

    add supported led device

--- src/main/java/pl/smarthouse/properties/SupportedZigbeeDevicesProperties.java
+++ src/main/java/pl/smarthouse/properties/SupportedZigbeeDevicesProperties.java
@@ -3,6 +3,6 @@ package pl.smarthouse.properties;
 import java.util.Set;
 
 public class SupportedZigbeeDevicesProperties {
-  public static Set<String> SUPPORTED_LIGHTS = Set.of("R9077");
+  public static Set<String> SUPPORTED_LIGHTS = Set.of("R9077", "4058075729025");
   public static Set<String> SUPPORTED_MOTION_SENSORS = Set.of("SNZB-03");
 }
